eae197ff42fad55b5b18d157e15b9a0d3b9d7ef7
Marcelo Alves <marcelo.asf92@gmail.com>
2023-12-26 23:37:45 -0300

    fix: Adding imports to the app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { OrdersModule } from './modules/orders/orders.module';
+import { LoginModule } from './modules/login/login.module';
+import { AuthMiddleware } from './middlewares/auth/auth.middleware';
+import { OrdersController } from './modules/orders/orders.controller';
+import { APP_FILTER } from '@nestjs/core';
+import { GlobalExceptionFilter } from './common/global-exception.filter';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule, OrdersModule, LoginModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(OrdersController);
+  }
+}
