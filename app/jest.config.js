58643f9756db8cf2d13d1f4863e4100ef084b4bf
Marcelo Alves <marcelo.asf92@gmail.com>
2023-12-26 23:58:25 -0300

    feat: Adding jest config

---
+++ app/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/path/to/non-test-files/",
+  ],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+};
